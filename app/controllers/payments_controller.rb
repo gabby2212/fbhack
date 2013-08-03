a632a5bc55e8e468fdf2733ff9ac48c0a779f9e0
Gaby Melchior <gdm2118@columbia.edu>
2013-08-03 14:30:59 -0400

    Doing controllers

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,12 @@
 class PaymentsController < ApplicationController
+	before_filter :require_current_user
+	
+	def index
+		@payments = current_user.payments
+	end
+
+	def create
+		@payment = current_user.payments.build
+		@payment.save
+	end
 end
