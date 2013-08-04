85d089bb5eb96d334f5683f382ffdd6f151ca112
ziyue <ziyue@moonlyt.com>
2013-08-03 22:02:18 -0400

    fix the index layout

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ VenOwe::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'welcome#payment'
 
   # See how all your routes lay out with "rake routes"
 
