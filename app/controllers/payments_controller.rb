9e7bb8795d49831dff313f4a352eb3e2e551f728
Gaby Melchior <gdm2118@columbia.edu>
2013-08-03 22:28:16 -0400

    fixing controllers

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,26 +6,27 @@ class PaymentsController < ApplicationController
 	end
 
 	def create
-		@relation = Relationship.find(:from_id => current_user.id && :to_id => params[:user_id])
-		@inverse_relation = Relationship.find(:from_id => params[:user_id] && :to_id => current_user.id)
+		@relation = Relationship.where(from_id: current_user.id, to_id: params[:receiver_id])
+		@inverse_relation = Relationship.find(from_id: params[:receiver_id], :to_id: current_user.id)
+		Rails.logger.debug("here")
 		if @relation
-			@relation.amount += params[:amount]
+		 	@relation.amount += params[:amount]
 		else
-			#no idea how to build this
-			current_user.Relationship.build()
+			@relation = Relationship.create(amount: params[:amount], from_id: current_user.id, to_id: params[:receiver_id])
+			@relation.save
 		end
 		if @inverse_relation
-			@inverse_relation.amount -= params[:amount]
+		 	@inverse_relation.amount -= params[:amount]
 		else
-			#build same thing as before with users other way around
-			current_user.Relationship.build()
+			@inverse_relation = Relationship.create(amount: params[:amount], from_id: params[:receiver_id], to_id: current_user.id)
+			@inverse_relation.save
 		end
 
-
-		@path_so_far = Path.initialize(params[:amount], [params[:user]])
-		@path_so_far = find_path(user, amount, @path_so_far)
-		@payment = current_user.payments.build
-		@payment.save
+		@payment = Payment.new
+		@payment.submit(params[:payment])
+		# @path_so_far = Path.new(params[:amount], [params[:user]])
+		# @path_so_far = find_path(user, amount, @path_so_far)
+		@payment
 	end
 
 	def find_path(user, amount, path)
@@ -55,7 +56,8 @@ class PaymentsController < ApplicationController
 		path.nodes.each do |n|
 			if index == path.nodes.length - 1
 				break
-			r = Relationship.find(:from_id => n.id && :to_id => path.nodes[index])
+			end
+			r = Relationship.where(:from_id => n.id)
 			r.amount = r.amount - path.amount
 			r.save
 		end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,3 @@
 class Relationship < ActiveRecord::Base
-
+	attr_accessible :amount, :from_id, :to_id
 end
