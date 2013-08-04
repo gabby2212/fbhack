d44ce14160e1289684eab5b7f69bce90d9f9df53
Howard Jing <howard.jing@gmail.com>
2013-08-04 09:33:26 -0400

    Revert "assets debug mode in production for arborjs"
    This reverts commit e8a8b5ea1d57437aa63196c2fd70bed63479a54c.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,6 @@ VenOwe::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # for arborjs
-  config.assets.debug = true
-
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
